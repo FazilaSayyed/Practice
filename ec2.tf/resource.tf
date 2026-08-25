37e9237a4e0b24c1dfd974f41d61be41f14a0161
FazilaSayyed <fazila.sayyed29@gmail.com>
2026-08-25 12:20:24 +0530

    aws

--- ec2.tf/resource.tf
+++ ec2.tf/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myec2" {
   ami = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro"
-  key_name = "aws.pem"
+  key_name = "aws"
   vpc_security_group_ids = ["sg-0172a65f910111ff6"]
    
 }
