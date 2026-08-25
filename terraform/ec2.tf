a3c7b737de1161358fc8ec9c78c0a638cf5f2e2e
FazilaSayyed <fazila.sayyed29@gmail.com>
2026-08-25 12:48:20 +0530

    c

---
+++ terraform/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "eu-north-1"
+    
+   }
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0aba19e56f3eaec05"
+  instance_type = "t3.micro"
+  key_name = "aws"
+  vpc_security_group_ids = ["sg-0172a65f910111ff6"]
+   
+}   
