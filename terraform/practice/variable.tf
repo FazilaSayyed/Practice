7046e14c341a7377a30d1a86bd6147e115dfb0bd
FazilaSayyed <fazila.sayyed29@gmail.com>
2026-09-02 06:39:49 +0530

    mention all values

---
+++ terraform/practice/variable.tf
@@ -0,0 +1,18 @@
+variable "myami" {
+    default = "ami id"
+} 
+variable "instance_type" {
+    default = "t3-micro"
+} 
+variable "key-name" {
+    default = "id-rsa"
+} 
+variable "mysg" {
+    default = "sg"
+}
+variable "project_name" {
+    default = "ec2_vpc"
+}
+variable "region" {
+    default = "us-east- "
+}   
