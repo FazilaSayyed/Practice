4c3d029ec9e1988150cbe1889a0c64c33074dcbd
FazilaSayyed <fazila.sayyed29@gmail.com>
2026-09-02 17:00:18 +0530

    sg

--- terraform/practice/vpc.tf
+++ terraform/practice/vpc.tf
@@ -47,13 +47,34 @@ resource "aws_route_table_association" "sub_associate" {
     route_table_id = aws_route_table.myroute.id
 }
 
+
+
+resource "aws_security_group" "mysg" {
+  name   = "my-security-group"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "myinstance" {
   ami = var.myami
   instance_type = var.instance_type
   key_name = var.key_name
   subnet_id = aws_subnet.mypubsub.id
 
-  vpc_security_group_ids = [mysg]
+  vpc_security_group_ids = [aws_security_group.mysg.id]
   
 
    tags = {
